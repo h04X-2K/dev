48610a5412ccd6c85c780b1fc06548cb1a6bb1c8
h04X <42872111+h04X-2K@users.noreply.github.com>
2021-04-07 06:37:19 +0200

    small changes

--- tpw/client.lua
+++ tpw/client.lua
@@ -1,4 +1,4 @@
-RegisterCommand('tpw', function(source, args, raw) --//Zum Wegpunkt Teleportieren
+RegisterCommand('tpw', function(source, args, raw) --//Teleport to the waypoint
     local playerPed = PlayerPedId() --//GetPlayerPed(-1)
     local waypoint = GetFirstBlipInfoId(8)
     local waypointCoords = GetBlipInfoIdCoord(waypoint)
